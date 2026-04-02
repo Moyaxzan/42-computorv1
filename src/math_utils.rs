0ee6924ad1ba51e3e57801751acd59ac5e9f8ed1
Tao <tao.spa2003@gmail.com>
2026-04-02 14:23:02 +0200

    [PROGRESS] display complex numbers with irreductible fractioins

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -1,11 +1,16 @@
 use core::f64;
-use std::fmt::Debug;
-
 
 pub struct Polynomial {
     pub a: f64,
     pub b: f64,
     pub c: f64
+}
+
+fn absi(val: i64) -> i64 {
+    if val < 0 {
+        return -val;
+    }
+    return val;
 }
 
 pub fn absf(val: f64) -> f64 {
@@ -44,48 +49,86 @@ pub fn one_r_solution(px: &Polynomial) -> f64 {
     return -px.b / (2.0 * px.a);
 }
 
-fn my_gcd(first: f64, second:f64) -> f64 {
-    if absf(first) < f64::EPSILON || absf(second) < f64::EPSILON {
-        return 1.0;
+fn my_gcd(first: i64, second:i64) -> i64 {
+    if first == 0 {
+        return second;
+    } else if second == 0 {
+        return first;
     }
-    let mut x = first;
-    let mut y = second;
-    while absf(y) > f64::EPSILON {
-        if y.round() < x.round() {
-            std::mem::swap(&mut x, &mut y);
-        }
-        y = y.round() % x.round();
+    if first < second {
+        return my_gcd(first, second % first);
+    } else {
+        return my_gcd(second, first % second)
     }
-    return x.round();
 }
 
-fn my_fmt(val: f64) -> String {
-    let rounded = (val * 1_000_000.0).round() / 1_000_000.0;
-    return format!("{}", rounded);
+fn dec_to_frac(dval: f64) -> (i64, i64) {
+    let int_val = dval.trunc();
+    let flt_val = dval - int_val;
+
+    let precision: i64 = 1_000_000;
+
+    let num = (flt_val * precision as f64).round() as i64;
+    let gcd = my_gcd(absi(num), absi(precision));
+
+    let num = num / gcd;
+    let den = precision / gcd;
+
+    let final_num = (int_val as i64) * den + num;
+
+    return (final_num, den);
+}
+
+fn irr_frac(num: f64, den: f64) -> (i64, i64) {
+    let num_fact = dec_to_frac(num);
+    let num_num: i64 = num_fact.0;
+    let num_den: i64 = num_fact.1;
+
+    let den_fact = dec_to_frac(den);
+    let den_num: i64 = den_fact.0;
+    let den_den: i64 = den_fact.1;
+
+    let gcd = my_gcd(num_num * den_den, num_den * den_num);
+    let mut res_num = num_num * den_den / gcd;
+    let mut res_den = num_den * den_num / gcd;
+    
+    if res_den < 0 {
+        res_num *= -1;
+        res_den *= -1;
+    }
+    return (res_num, res_den);
 }
 
 pub fn c_solutions(px: &Polynomial, delta: &f64) -> String {
-    let mut real_numerator: f64 = - px.b;
-    let mut real_denominator: f64 = 2.0 * px.a;
-    let mut comp_numerator: f64 = sqrtf(absf(*delta));
-    let mut comp_denominator: f64 = 2.0 * px.a;
+    let real_frac = irr_frac(- px.b, 2.0 * px.a);
+    let comp_frac = irr_frac(sqrtf(absf(*delta)), 2.0 * px.a);
 
-    let real_gcd = my_gcd(absf(real_numerator), absf(real_denominator));
-    let comp_gcd = my_gcd(absf(comp_numerator), absf(comp_denominator));
+    let real_num = real_frac.0;
+    let real_den = real_frac.1;
+    let comp_num = comp_frac.0;
+    let comp_den = comp_frac.1;
 
-    real_numerator /= real_gcd;
-    real_denominator /= real_gcd;
-    comp_numerator /= comp_gcd;
-    comp_denominator /= comp_gcd;
+    let real = if real_den == 1 {
+        format!("{}", real_num)
+    } else if comp_den == -1 {
+        format!("{}i", -real_num)
+    } else {
+        format!("{}/{}", real_num, real_den)
+    };
+
+    let comp = if comp_den == 1 {
+        format!("{}i", comp_num)
+    } else if comp_den == -1 {
+        format!("{}i", -comp_num)
+    } else {
+        format!("{}i/{}", comp_num, comp_den)
+    };
+
     return format!(
-        "{}/{} + {}i/{}\n{}/{} - {}i/{}",
-        my_fmt(real_numerator),
-        my_fmt(real_denominator),
-        my_fmt(comp_numerator),
-        my_fmt(comp_denominator),
-        my_fmt(real_numerator),
-        my_fmt(real_denominator),
-        my_fmt(comp_numerator),
-        my_fmt(comp_denominator)
+        "{} + {}\n{} - {}",
+        real,
+        comp,
+        real,
+        comp
     );
 }
